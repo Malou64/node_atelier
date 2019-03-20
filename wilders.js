2c1eaf74e7c2a75b6843b9f5e371ccb1635e19af
Marlene Angouillant <marlene.angouillant@gmail.com>
2019-03-20 19:28:10 +0100

    first commit

---
+++ wilders.js
@@ -0,0 +1,7 @@
+const chalk = require ('chalk');
+
+console.log(chalk.bold.italic.bgRed.white('MY AWESOME CREW:'));
+console.log(chalk.bold.bgWhite.blue('Sylène'));
+console.log(chalk.bgMagenta.yellow('Cloe'));
+console.log(chalk.bgWhiteBright.magenta('Stephane'));
+console.log(chalk.bgYellow.green('Marlene'));
